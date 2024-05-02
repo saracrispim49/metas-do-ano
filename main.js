2428a5a4c43f833a1179058c24878593727a326c
saracrispim49 <106817772+saracrispim49@users.noreply.github.com>
2024-05-01 21:33:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-07-21T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-05-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-12T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-20T00:00:00");
+const tempoObjetivo3 = new Date("2025-06-01T00:00:00");
+const tempoObjetivo4 = new Date("2025-06-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
